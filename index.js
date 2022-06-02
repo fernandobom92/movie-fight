40c64f6d24daa4576f7a49a5316d8728ccb3389a
fernandobom92 <106743747+fernandobom92@users.noreply.github.com>
2022-06-02 08:59:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//this is the version 1.01
+//this is the version 1.00
 const fetchData = async (searchTerm) => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
